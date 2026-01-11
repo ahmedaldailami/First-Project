2fa2d03361ee0e01732630ebb86c881962687868
ahmedaldailami <ahmedadnan2024@gmail.com>
2026-01-12 02:13:19 +0300

    text on hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+// import { useRef } from "react";
 import Particles from "./components/Particles"
 import PillNav from "./components/PillNav"
+import VariableProximity from "./components/text/VariableProximity";
 
 
 
 
 function App() {
+  // const containerRef = useRef(null);
+
+
+
+
+
+
+
+
+
+
   return (
     <>
       {/*            Background            */}
@@ -42,6 +55,26 @@ function App() {
           pillTextColor="#000000"
         />
       </header>
+
+
+{/* 
+      <div
+        ref={containerRef}
+        style={{ position: 'relative' }}
+      >
+        <VariableProximity
+          label={'Hover me! And then star React Bits on GitHub, or else...'}
+          className={'variable-proximity-demo'}
+          fromFontVariationSettings="'wght' 400, 'opsz' 9"
+          toFontVariationSettings="'wght' 1000, 'opsz' 40"
+          containerRef={containerRef}
+          radius={100}
+          falloff='linear'
+        />
+      </div> */}
+
+
+
 
 
 
