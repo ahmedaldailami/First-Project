644515baa1fe2bd946fb3c5a16ff81c36f42062d
ahmedaldailami <ahmedadnan2024@gmail.com>
2026-01-14 00:41:49 +0300

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-// import { useRef } from "react";
+import { useEffect, useState } from "react";
+import { HeroSection } from "./components/HeroSection"
 import Particles from "./components/Particles"
 import PillNav from "./components/PillNav"
-import VariableProximity from "./components/text/VariableProximity";
+import { AnimatePresence, motion } from "framer-motion";
+// import { Sun, Moon } from "lucide-react";
 
 
 
 
 function App() {
-  // const containerRef = useRef(null);
 
 
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
+
+
+  // Theme toggle
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
 
 
@@ -56,21 +71,63 @@ function App() {
       </header>
 
 
-      {/* 
-      <div
-        ref={containerRef}
-        style={{ position: 'relative' }}
+      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")} >
+        {theme}
+      </button>
+      
+
+      {/* Theme Toggle Button */}
+      <div className="bg-amber-400">
+
+      <motion.button
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        className="p-2 bg-amber-50 rounded-full text-sm font-semibold
+              hover:bg-pink-400 dark:hover:bg-pink-800 transition-colors
+               hidden md:block"
+        // whileHover={{ scale: 1.1 }}
+        // whileTap={{ scale: 0.9 }}
       >
-        <VariableProximity
-          label={'Hover me! And then star React Bits on GitHub, or else...'}
-          className={'variable-proximity-demo'}
-          fromFontVariationSettings="'wght' 400, 'opsz' 9"
-          toFontVariationSettings="'wght' 1000, 'opsz' 40"
-          containerRef={containerRef}
-          radius={100}
-          falloff='linear'
-        />
-      </div> */}
+
+        {/* <AnimatePresence mode="wait" initial={false}> */}
+          {theme === "dark" ? (
+            <motion.div
+              key="moon"
+              initial={{ y: -20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 20, opacity: 0 }}
+              transition={{ duration: 0.2 }}
+            >
+              
+              {/* <Moon size={20} className="text-gray-800 dark:text-white" /> */}
+              <span>dark</span>
+            </motion.div>
+          ) : (
+            <motion.div
+              key="sun"
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -20, opacity: 0 }}
+              transition={{ duration: 0.2 }}
+            >
+              {/* <Sun size={20} className="text-gray-800 dark:text-white" /> */}
+                <span>light</span>
+
+            </motion.div>
+          )}
+        {/* </AnimatePresence> */}
+      </motion.button>
+
+      </div>
+
+
+
+
+      <HeroSection />
+
+      {/* <section className="container m-auto min-h-dvh flex items-center ">
+        
+      
+      </section> */}
 
 
 
@@ -78,7 +135,8 @@ function App() {
 
 
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem officia iusto sunt aliquid ex doloremque molestiae, vero esse saepe? Ab quia adipisci eligendi nulla. Unde illo provident earum modi suscipit.</p>
+
+
     </>
   )
 }
